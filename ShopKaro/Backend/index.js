093e80578a37b8fc6143b250b70efb948a87a474
Nikhil <ns7970506@gmail.com>
2026-09-02 23:49:35 +0530

    message

---
+++ ShopKaro/Backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('ShopKaro Backend is working properly')
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    
+})
